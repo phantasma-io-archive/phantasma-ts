87302787566cd0033c1d779674d70ceb460d60bd
Noah <ncwardell@gmail.com>
2021-11-01 03:39:25 -0700

    modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,48 @@
 import * as api from './core/index';
 
+class hostConfiguration {
 
-class phantasma{
+    //Variable Initilizers
+    rpc: string;
+    peerList: string;
+    nexus: string;
+    chain: string;
 
-    rpc = new api.PhantasmaAPI('https://seed.ghostdevs.com:7077/rpc', 'https://ghostdevs.com/getpeers.json');
+    constructor(
+        
+        //Set Defualt Values
+        rpcURL: string = 'https://seed.ghostdevs.com:7077/rpc',
+        jsonPeerList: string = 'https://ghostdevs.com/getpeers.json',
+        nexus: string = 'mainnet',
+        chain: string = 'main'
+
+    ) {
+
+        //Set Values
+        this.rpc = rpcURL;
+        this.peerList = jsonPeerList;
+        this.nexus = nexus;
+        this.chain = chain;
+
+    };
+
+};
 
 
+class phantasma {
 
-    
+    //Initillize Some Variables
+    config: hostConfiguration;
+    rpc: api.PhantasmaAPI;
 
+    //Constructs The BigBoi
+    constructor(config: hostConfiguration = new hostConfiguration) {
+        
+        //Sets The Config
+        this.config = config;
 
-
+        //Sets The RPC Host
+        this.rpc = new api.PhantasmaAPI(this.config.rpc, this.config.peerList);
+    };
 
 };
