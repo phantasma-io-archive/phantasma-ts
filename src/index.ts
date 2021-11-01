7f6b0de077a723c62ee85917a76d98db868cd429
Noah <ncwardell@gmail.com>
2021-11-01 02:38:09 -0700

    modified:   src/index.ts 	modified:   tsconfig.json

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import * as api from './core/index';
+
+
+class phantasma{
+
+    rpc = new api.PhantasmaAPI('https://seed.ghostdevs.com:7077/rpc', 'https://ghostdevs.com/getpeers.json');
+
+
+
+    
+
+
+
+
+};
