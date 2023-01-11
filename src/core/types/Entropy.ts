810b0b67ab207d80995436d57fa91f891a5448f1
João Novo <34069419+TeknoPT@users.noreply.github.com>
2023-01-11 15:37:19 +0000

    Fix issue

--- src/core/types/Entropy.ts
+++ src/core/types/Entropy.ts
@@ -1,7 +1,7 @@
-import * as crypto from 'crypto';
+import crypto from 'crypto';
 
 export class Entropy {
-    private static rnd = crypto.randomBytes(24);
+    //private static rnd = crypto.randomBytes(24);
     public static GetRandomBytes(targetLength: number): Buffer {
         return crypto.randomBytes(targetLength);
     }
