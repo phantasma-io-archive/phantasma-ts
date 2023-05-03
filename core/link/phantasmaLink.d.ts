92b3da93ea36429d6bed45f6de722464e1fd4382
João Novo <34069419+TeknoPT@users.noreply.github.com>
2023-05-03 08:49:35 +0100

    Update Phantasma Link to the have an account definition.

--- core/link/phantasmaLink.d.ts
+++ core/link/phantasmaLink.d.ts
@@ -1,3 +1,4 @@
+import { Balance } from "../rpc/phantasma";
 export declare enum ProofOfWork {
     None = 0,
     Minimal = 5,
@@ -6,17 +7,33 @@ export declare enum ProofOfWork {
     Heavy = 24,
     Extreme = 30
 }
+export interface IFile {
+    name: string;
+    hash: string;
+    size: number;
+    date: string;
+}
+export interface IAccount {
+    alias: string;
+    name: string;
+    address: string;
+    avatar: string;
+    platform: string;
+    external: string;
+    balances: Balance[];
+    files: IFile[];
+}
 export declare class PhantasmaLink {
     host: string;
     dapp: any;
     onLogin: (succ: any) => void;
     providerHint: any;
-    onError: any;
+    onError: (message: any) => void;
     socket: any;
     requestCallback: any;
     token: any;
     requestID: number;
-    account: any;
+    account: IAccount;
     wallet: any;
     messageLogging: boolean;
     version: number;
@@ -24,23 +41,23 @@ export declare class PhantasmaLink {
     chain: string;
     platform: string;
     constructor(dappID: any, logging?: boolean);
-    onMessage: (msg: any) => void;
-    login(onLoginCallback: any, onErrorCallback: any, version?: number, platform?: string, providerHint?: string): void;
-    invokeScript(script: any, callback: any): void;
-    signTx(script: any, payload: string | null, callback: (arg0: any) => void, onErrorCallback: () => void, pow?: ProofOfWork, signature?: string): void;
-    signTxSignature(tx: any, callback: any, onErrorCallback: any, signature?: string): void;
-    multiSig(subject: any, callback: any, onErrorCallback: any, signature?: string): void;
-    getPeer(callback: any, onErrorCallback: any): void;
-    fetchWallet(callback: any, onErrorCallback: any): void;
-    getNexus(callback: any, onErrorCallback: any): void;
-    signData(data: any, callback: any, onErrorCallback: any, signature?: string): void;
-    createSocket(): void;
+    onMessage: (msg: string) => void;
+    login(onLoginCallback: (success: boolean) => void, onErrorCallback: (message: string) => void, version?: number, platform?: string, providerHint?: string): void;
+    invokeScript(script: string, callback: (message: string) => void): void;
+    signTx(script: any, payload: string | null, callback: (arg0: string) => void, onErrorCallback: () => void, pow?: ProofOfWork, signature?: string): void;
+    signTxSignature(tx: string, callback: (result: string) => void, onErrorCallback: () => void, signature?: string): void;
+    multiSig(subject: string, callback: (result: string) => void, onErrorCallback: () => void, signature?: string): void;
+    getPeer(callback: (result: string) => void, onErrorCallback: () => void): void;
+    fetchWallet(callback: (result: any) => void, onErrorCallback: (message: any) => void): void;
+    getNexus(callback: (message: any) => void, onErrorCallback: (message: any) => void): void;
+    signData(data: string, callback: (success: string) => void, onErrorCallback: (message: string) => void, signature?: string): void;
+    createSocket(isResume?: boolean): void;
     toggleMessageLogging(): void;
     resume(token: any): void;
     retry(): void;
     set dappID(dapp: any);
     get dappID(): any;
-    sendLinkRequest(request: any, callback: any): void;
-    disconnect(triggered: any): void;
+    sendLinkRequest(request: string, callback: (T: any) => void): void;
+    disconnect(triggered: string | boolean | undefined): void;
 }
 //# sourceMappingURL=phantasmaLink.d.ts.map
